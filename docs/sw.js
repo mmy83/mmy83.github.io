281caab449b8b734cc343a6c7bc8bd6d9ab389ab
mmy83 <mmy83@126.com>
2023-02-07 20:18:43 +0800

    增加首页列表项的分类连接

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230207.201348';
+const cacheName = 'chirpy-20230207.201817';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
