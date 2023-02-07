92d5d9f1ad2198707a64e6202775ca3d5760e489
mmy83 <mmy83@126.com>
2023-02-07 20:32:05 +0800

    修正注释，另一种连接方法

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230207.202832';
+const cacheName = 'chirpy-20230207.203117';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
