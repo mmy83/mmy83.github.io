f585866cf5d820ab28ae8939f2d41b8b057608ea
mmy83 <mmy83@126.com>
2023-02-07 20:14:43 +0800

    修改账号、分享、作者信息

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230207.201020';
+const cacheName = 'chirpy-20230207.201348';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
